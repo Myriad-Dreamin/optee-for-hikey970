03817cbc71ce7cacfbe62cd780cedf33332119d2
Myriad-Dreamin <camiyoru@gmail.com>
2020-08-23 14:05:34 +0800

    init

---
+++ scripts/build.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+if __name__ == '__main__':
+    print("build script")
+
+
---
+++ scripts/commands.mk
@@ -0,0 +1,8 @@
+# Hikey Build Commands
+
+
+build:
+	@optee-for-hikey970/scripts/build.py
+
+
+.PHONY: build
